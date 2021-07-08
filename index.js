14e6ac6ef4948b5925e4453737cf2b341a6ea34d
Rachealelias <rachealsamuel708@gmail.com>
2021-07-07 22:25:58 -0400

    commit tree d160dcd426312a9a9caf5cdc07e42548f3b83ecc

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const result = drivers.filter(driver =>
+         driver.toLowerCase() === name.toLowerCase());
+    //console.log(result);
+    return result;
+
+}
+
+function fuzzyMatch(drivers, initials){
+   // console.log(drivers);
+      return drivers.filter(driver =>
+         driver.toLowerCase().indexOf(initials.toLowerCase()) === 0);
+}
+
+function matchName(drivers, name){
+    const ans = drivers.filter(driver => driver.name === name);
+   // console.log(ans);
+   return ans;
+}
+
+
